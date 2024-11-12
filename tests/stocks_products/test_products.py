83e4866c4a1d07744d3a3f0d28b78ebfcc01ddbf
inncent <innokentiykim90@gmail.com>
2024-11-12 18:49:36 +0500

    test added

---
+++ tests/stocks_products/test_products.py
@@ -0,0 +1,12 @@
+import pytest
+from rest_framework.test import APIClient
+
+
+PRODUCTS_URL = "/api/v1/products/"
+
+
+@pytest.mark.django_db
+def test_products(client, courses_factory):
+    client = APIClient()
+    response = client.get(PRODUCTS_URL)
+    assert response.status_code == 200
--- tests/test.py
+++
@@ -1,2 +0,0 @@
-
-assert True is True
